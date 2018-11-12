3249b581b16ec29d4d3579f394f89e1e99a21035
vpjacob <824629419@qq.com>
2018-11-12 22:43:41 +0800

    update react-navigation & react-redux

--- src/screen/AppWithNavigationState.js
+++ src/screen/AppWithNavigationState.js
@@ -1,21 +1,20 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import {addNavigationHelpers} from 'react-navigation';
-import {createReactNavigationReduxMiddleware,createReduxBoundAddListener} from 'react-navigation-redux-helpers';
+import {createReactNavigationReduxMiddleware,reduxifyNavigator} from 'react-navigation-redux-helpers';
 import {AppRoute} from './screenIndex';
 
 createReactNavigationReduxMiddleware('root',state => state.nav);
-const addListener = createReduxBoundAddListener('root');
+const addListener = reduxifyNavigator(AppRoute,'root');
 
 class AppWithNavigationState extends Component {
     render () {
         const {dispatch,nav} = this.props;
         return (
-          <AppRoute navigator={addNavigationHelpers({
+          <AppRoute navigator={{
               dispatch:dispatch,
               state:nav,
               addListener
-          })}
+          }}
           />
         );
     }
